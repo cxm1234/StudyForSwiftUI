416b779d4d9db522773221762c54adfa2d3992be
245413040 <245413040@qq.com>
2022-02-10 00:22:47 +0800

    start of UI

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,17 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚞", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    
+    @State var emojiCount = 4
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding(.all)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack {
+                Button(action: {
+                    emojiCount -= 1
+                }, label: {
+                    VStack {
+                        Text("Remove")
+                        Text("Card")
+                    }
+                })
+                Spacer()
+                Button(action: {
+                    emojiCount += 1
+                }, label: {
+                    VStack {
+                        Text("Add")
+                        Text("Card")
+                    }
+                })
+            }
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
 
     }
     
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
     
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
