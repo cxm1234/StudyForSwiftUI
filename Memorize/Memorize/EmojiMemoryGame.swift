03139d5e250bf8cc273b95372c89ea442aa029c2
245413040 <245413040@qq.com>
2022-02-14 00:10:01 +0800

    MVVM stucual

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by xiaoming on 2022/2/13.
+//
+
+import SwiftUI
+
+
+
+class EmojiMemoryGame {
+    
+    static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚞", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCard: 4) { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
