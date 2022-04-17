dd4a9fd9fe9208c5631c51d4c64925e0e5b71da1
245413040 <245413040@qq.com>
2022-04-18 00:01:31 +0800

    in old way

--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -13,6 +13,8 @@ class EmojiArtDocument: ObservableObject {
     
     init() {
         emojiArt = EmojiArtModel()
+        emojiArt.addEmoji("😀", at: (-200, -100), size: 80)
+        emojiArt.addEmoji("😍", at: (50, 100), size: 40)
     }
     
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
